c1da1a013bd3180ed1c8c9188645fbc118717012
Bunny Bites <s.mukeshkr481@gmail.com>
2023-09-19 21:26:21 +0530

    handle api fetch and response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    // println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let api_url = "https://cat-fact.herokuapp.com/facts/";
+
+    let response = get_mock_response(api_url).await;
+
+    match response {
+        Ok(fetched_response) => {
+            dbg!(fetched_response);
+        }
+        Err(_) => {
+            panic!("Response Fetch Error");
+        }
+    }
+}
+
+async fn get_mock_response(url: &str) -> Result<serde_json::Value, reqwest::Error> {
+    let response = reqwest::get(url).await?.json::<serde_json::Value>().await?;
+
+    Ok(response)
 }
